a06a8cec89f3d2491d04382b7da0ae4d203081a9
Srijato Das <135578874+Srijato-05@users.noreply.github.com>
2026-03-16 23:42:43 +0530

    Delete Project Assignment directory

--- Project Assignment/database/init.sql
+++
@@ -1,7 +0,0 @@
--- Init script for PostgreSQL
--- Webapp DB is created natively by the POSTGRES_DB environment variable
--- Here we can add custom extensions or roles
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Additional setup can be performed here if necessary
